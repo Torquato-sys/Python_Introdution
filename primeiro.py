8755413599b48edb344c48e507553aead1700e4c
Mateus Torquato Fernandes <mateustorquato98@gmail.com>
2023-07-31 23:55:07 -0300

    Primeiro Projeto

---
+++ primeiro.py
@@ -0,0 +1,6 @@
+# Primeiro Codigo
+
+print ("Hello, World!") # Imprimir palavras, Texto(string);
+print ('Seja Bem Vindo')
+
+input ('Você ama a Nathalia Cristinne?') # input, entrada de resposta fornecida pelo usuario;
