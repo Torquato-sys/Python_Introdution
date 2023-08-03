6a8e436af4d3334c10483d00dbea450da6a7f8b5
Mateus Torquato Fernandes <mateustorquato98@gmail.com>
2023-08-03 04:40:12 -0300

    Funções

---
+++ funções.py
@@ -0,0 +1,15 @@
+# > FUNÇÕES
+
+# 1. O que são funções e por que utilizá-las?
+
+# Funlçoes que ja utilizamos anteriormente...
+
+print() # - Imprimi uma mensagem (int, float, str) no console (terminal, cmd);
+input() # - Retorna um dado informado pelo usuário (entrada padrão), podendo ser armazenado os dados digitados pelo usuario em uma variavel;
+len() # - Recebe uma lista e retorna o tamanho da mesma;
+max() # - Retorna o maior valor da lista informada;
+
+# 2- Criação de funções
+
+# Função inicial
+
